8e63ddd76364a4ab91c4bc50060674c398f840a3
James Manton <ajd.manton@googlemail.com>
2014-03-25 20:02:24 +0000

    Document arguments for plot3d.BrainTemplate

--- R/nat.misc-plotting.r
+++ R/nat.misc-plotting.r
@@ -1,3 +1,9 @@
+#' Plot a 3D surface of a template brain
+#'
+#' @param x the template brain to plot.
+#' @param col the color of the surface.
+#' @param alpha the alpha value of the surface.
+#' @param ... extra arguments to pass to \code{\link[rgl]{plot3d}}.
 #' @export
 #' @method plot3d BrainTemplate
 #' @importFrom rgl plot3d
