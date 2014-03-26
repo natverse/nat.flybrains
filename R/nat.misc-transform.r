f8f20fcc9f0f94c0144cdbf44f7917f9bc6f158a
James Manton <ajd.manton@googlemail.com>
2014-03-26 10:59:04 +0000

    Move mirroring function into transform.r file

--- R/nat.misc-transform.r
+++ R/nat.misc-transform.r
@@ -13,3 +13,22 @@ xform.BrainTemplate <- function(x, x2, data, transform=c("warp",'affine')) {
   reg <- system.file(package="nat.misc", paste0("extdata/bridgingregistrations/", deparse(substitute(x)), "_", deparse(substitute(x2)), ".list"))
   xform(data, reg=reg, transform=transform)
 }
+
+#' Mirror 3D object around a given axis, optionally using a warping registration
+#'
+#' @param x template brain (e.g. IS2).
+#' @param data the 3D object to be mirrored.
+#' @param mirrorAxis the axis to mirror (default \code{"X"}).
+#' @param transform whether to use warp (default) or affine component of
+#'   registration, or simply flip about midplane of axis.
+#' @importFrom nat mirror
+#' @method mirror BrainTemplate
+#' @aliases mirror
+#' @export
+mirror.BrainTemplate <- function(x, data, mirrorAxis=c("X","Y","Z"), transform=c("warp",'affine','flip')) {
+  warpfile <- x$mirrorLoc()
+  mirrorAxis <- match.arg(mirrorAxis)
+  axisCol <- which(mirrorAxis == c("X", "Y", "Z"))
+  mirrorAxisSize <- x$BoundingBox[2, axisCol] - x$BoundingBox[1, axisCol]
+  mirror(data, mirrorAxisSize=mirrorAxisSize, mirrorAxis=mirrorAxis, warpfile=warpfile)
+}
