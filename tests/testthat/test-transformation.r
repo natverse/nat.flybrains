3a1d69cf5d78da6c89d915c3cb02d2576ef4de47
James Manton <ajd.manton@googlemail.com>
2014-03-26 16:57:33 +0000

    Tidy transformation tests

--- tests/testthat/test-transformation.r
+++ tests/testthat/test-transformation.r
@@ -1,8 +1,9 @@
 context("Transformation")
 
+points <- matrix(c(30, 30, 30, 110, 110, 110), ncol=3, byrow=T)
+colnames(points) <- c("X", "Y", "Z")
+
 test_that("can mirror FCWB", {
-  points <- matrix(c(30, 30, 30, 110, 110, 110), ncol=3, byrow=T)
-  colnames(points) <- c("X", "Y", "Z")
   points.mirror <- mirror(FCWB, points)
 
   points.mirrorexp <- matrix(c(533.852185, 453.736346, 29.7033157, 110.009167, 29.9916627, 107), ncol=3)
@@ -12,8 +13,6 @@ test_that("can mirror FCWB", {
 })
 
 test_that("can mirror JFRC2", {
-  points <- matrix(c(30, 30, 30, 110, 110, 110), ncol=3, byrow=T)
-  colnames(points) <- c("X", "Y", "Z")
   points.mirror <- mirror(JFRC2, points)
 
   points.mirrorexp <- matrix(c(601.542797, 522.999806, 12.090383, 97.4817247, 17.7922986, 103.485149), ncol=3)
@@ -23,8 +22,6 @@ test_that("can mirror JFRC2", {
 })
 
 test_that("can mirror IS2", {
-  points <- matrix(c(30, 30, 30, 110, 110, 110), ncol=3, byrow=T)
-  colnames(points) <- c("X", "Y", "Z")
   points.mirror <- mirror(IS2, points)
 
   points.mirrorexp <- matrix(c(284.682251, 204.314929, 30.2698099, 110.658494, 30.1569345, 110.068235), ncol=3)
@@ -34,8 +31,6 @@ test_that("can mirror IS2", {
 })
 
 test_that("can bridge JFRC2 to FCWB", {
-  points <- matrix(c(30, 30, 30, 110, 110, 110), ncol=3, byrow=T)
-  colnames(points) <- c("X", "Y", "Z")
   points.bridge <- xform(JFRC2, FCWB, points)
 
   points.bridgeexp <- matrix(c(23.1763202, 92.8401558, 35.5010956, 115.450817, 29.7029072, 85.9619354), ncol=3)
