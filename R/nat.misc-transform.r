cd76438cb79bb778ec142fd2b30c5fd6cdfa2327
James Manton <ajd.manton@googlemail.com>
2014-03-31 12:02:52 +0100

    Add {xform, mirror}_brain() and remove {xform, mirror}.BrainTemplate()

--- R/nat.misc-transform.r
+++ R/nat.misc-transform.r
@@ -1,42 +1,42 @@
+brain2reg <- function(reference, sample, mirror=FALSE) {
+  if(mirror) {
+    reg <- extdata(paste0("mirroringregistrations/", reference$regName, "_mirror.list"))
+  } else {
+    reg <- extdata(paste0("bridgingregistrations/", reference$regName, "_", sample$regName, ".list"))
+  }
+  reg
+}
+
 #' Transform 3D object from one template brain to another, optionally using a warping registration
 #'
-#' @param template1 source template brain (e.g. IS2) that data is in.
-#' @param template2 target template brain (e.g. IS2) that data should be transformed into.
-#' @param data the 3D object to be mirrored.
-#' @param transform whether to use warp (default) or affine component of
-#'   registration.
+#' @param x the 3D object to be mirrored.
+#' @param reference target template brain (e.g. IS2) that data should be transformed into.
+#' @param sample source template brain (e.g. IS2) that data is currently in.
 #' @param ... extra arguments to pass to \code{\link[nat]{xform}}.
-#' @importFrom nat xform
 #' @export
-trans <- function(template1, template2, data, transform=c('warp', 'affine'), ...) {
-  direction <- "inverse"
-  reg <- extdata(paste0("bridgingregistrations/", deparse(substitute(template2)), "_", deparse(substitute(template1)), ".list"))
+xform_brain <- function(x, reference, sample, ...) {
+  direction <- 'inverse'
+  reg <- brain2reg(reference, sample)
   if(reg == "") {
-    # We have no registration in this direction, so try inverting one in the other direction
-    direction <- "forward"
-    reg <- extdata(paste0("bridgingregistrations/", deparse(substitute(template1)), "_", deparse(substitute(template2)), ".list"))
+    reg <- brain2reg(sample, reference)
     if(reg == "") stop("No suitable registration found.")
-    warning("Numerically inverting registration from ", deparse(substitute(template1)), " to ", deparse(substitute(template2)), ". This may take some time and results may be inaccurate.")
+    warning("Numerically inverting registration from ", reference$regName, " to ", sample$regName, ". This may take some time and results may be inaccurate.")
+    direction <- 'forward'
   }
-  xform(data, reg=reg, transform=transform, direction=direction, ...)
+  nat::xform(x, reg=reg, direction=direction, ...)
 }
 
 #' Mirror 3D object around a given axis, optionally using a warping registration
 #'
-#' @param x template brain (e.g. IS2).
-#' @param data the 3D object to be mirrored.
+#' @param x the 3D object to be mirrored.
+#' @param brain source template brain (e.g. IS2) that data is in.
 #' @param mirrorAxis the axis to mirror (default \code{"X"}).
-#' @param transform whether to use warp (default) or affine component of
-#'   registration, or simply flip about midplane of axis.
-#' @param extra arguments to pass.
-#' @importFrom nat mirror
-#' @method mirror BrainTemplate
-#' @aliases mirror
+#' @param ... extra arguments to pass to \code{\link[nat]{xform}}.
 #' @export
-mirror.BrainTemplate <- function(x, data, mirrorAxis=c("X","Y","Z"), transform=c("warp",'affine','flip'), ...) {
-  warpfile <- extdata(paste0("mirroringregistrations/", deparse(substitute(x)), "_mirror.list"))
+mirror_brain <- function(x, brain, mirrorAxis=c("X","Y","Z"), ...) {
+  warpfile <- brain2reg(reference=brain, mirror=TRUE)
   mirrorAxis <- match.arg(mirrorAxis)
   axisCol <- which(mirrorAxis == c("X", "Y", "Z"))
-  mirrorAxisSize <- x$BoundingBox[2, axisCol] - x$BoundingBox[1, axisCol]
-  mirror(data, mirrorAxisSize=mirrorAxisSize, mirrorAxis=mirrorAxis, warpfile=warpfile)
+  mirrorAxisSize <- brain$BoundingBox[2, axisCol] - brain$BoundingBox[1, axisCol]
+  nat::mirror(x, mirrorAxisSize=mirrorAxisSize, mirrorAxis=mirrorAxis, warpfile=warpfile)
 }
--- tests/testthat/test-transformation.r
+++ tests/testthat/test-transformation.r
@@ -4,7 +4,7 @@ points <- matrix(c(30, 30, 30, 110, 110, 110), ncol=3, byrow=T)
 colnames(points) <- c("X", "Y", "Z")
 
 test_that("can mirror FCWB", {
-  points.mirror <- mirror(FCWB, points)
+  points.mirror <- mirror_brain(points, FCWB)
 
   points.mirrorexp <- matrix(c(533.852185, 453.736346, 29.7033157, 110.009167, 29.9916627, 107), ncol=3)
   colnames(points.mirrorexp) <- c("X", "Y", "Z")
@@ -13,7 +13,7 @@ test_that("can mirror FCWB", {
 })
 
 test_that("can mirror JFRC2", {
-  points.mirror <- mirror(JFRC2, points)
+  points.mirror <- mirror_brain(points, JFRC2)
 
   points.mirrorexp <- matrix(c(601.542797, 522.999806, 12.090383, 97.4817247, 17.7922986, 103.485149), ncol=3)
   colnames(points.mirrorexp) <- c("X", "Y", "Z")
@@ -22,7 +22,7 @@ test_that("can mirror JFRC2", {
 })
 
 test_that("can mirror IS2", {
-  points.mirror <- mirror(IS2, points)
+  points.mirror <- mirror_brain(points, IS2)
 
   points.mirrorexp <- matrix(c(284.682251, 204.314929, 30.2698099, 110.658494, 30.1569345, 110.068235), ncol=3)
   colnames(points.mirrorexp) <- c("X", "Y", "Z")
@@ -31,7 +31,7 @@ test_that("can mirror IS2", {
 })
 
 test_that("can bridge JFRC2 to FCWB", {
-  points.bridge <- trans(JFRC2, FCWB, points)
+  points.bridge <- xform_brain(points, FCWB, JFRC2)
 
   points.bridgeexp <- matrix(c(23.1763202, 92.8401558, 35.5010956, 115.450817, 29.7029072, 85.9619354), ncol=3)
   colnames(points.bridgeexp) <- c("X", "Y", "Z")
@@ -40,7 +40,7 @@ test_that("can bridge JFRC2 to FCWB", {
 })
 
 test_that("can bridge JFRC2 to IS2 using inverse registration", {
-  points.bridge <- trans(JFRC2, IS2, points)
+  points.bridge <- xform_brain(points, IS2, JFRC2)
 
   points.bridgeexp <- matrix(c(-76.9174206, -14.0714713, 44.4391667, 121.076295, 37.566708, 150.794994), ncol=3)
   colnames(points.bridgeexp) <- c("X", "Y", "Z")
