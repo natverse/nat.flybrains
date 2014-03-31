2177d9a271721abc2c3c09ccbeee8a51dae3f1ac
James Manton <ajd.manton@googlemail.com>
2014-03-31 14:00:11 +0100

    Remove plot3d() export

--- R/nat.misc-plotting.r
+++ R/nat.misc-plotting.r
@@ -7,7 +7,6 @@
 #' @export
 #' @method plot3d BrainTemplate
 #' @importFrom rgl plot3d
-#' @export plot3d
 plot3d.BrainTemplate <- function(x, col='grey', alpha=0.3, ...) {
   plot3d(get(paste0(deparse(substitute(x)), ".surf")), col=col, alpha=alpha, ...)
 }
