a6e547b20e97d1e9e2b2afe54b7cdb86d86a5121
James Manton <ajd.manton@googlemail.com>
2014-05-02 16:46:36 +0100

    Add skeleton of package documentation

---
+++ R/PACKAGE.r
@@ -0,0 +1,27 @@
+#' Additional flybrain-specific data and tools for use with the NeuroAnatomy Toolbox (nat)
+#'
+#' This package provides data, such as template brain information and
+#' registrations, along with extra functions to extend nat towards easy use for
+#' researchers working with fly data.
+#'
+#' @section Template brains:
+#' Information on a number of template brains is provided.
+#'
+#' @section Bridging registrations:
+#' A number of registrations for bridging data from one template to another are
+#' provided.
+#'
+#' @section Mirroring registrations:
+#' A number of registrations for mirroring data from one side of a template
+#' brain to the other are provided.
+#'
+#' @section Helper functions:
+#' Easy-to-use functions for transforming data from one template brain to
+#' another, displaying slices alongside 3D data, etc. are provided.
+#'
+#' @name nat.flybrains-package
+#' @aliases nat.flybrains
+#' @seealso \code{\link[nat]{nat}}
+#' @docType package
+#' @keywords package
+NULL
