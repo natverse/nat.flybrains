c544ba4be05f9781f42dcff1df5f4e8f5f05a25a
Gregory Jefferis <jefferis@gmail.com>
2014-06-20 19:01:47 +0100

    tests to confirm we can use extra registrations

--- tests/testthat/test-transformation.r
+++ tests/testthat/test-transformation.r
@@ -47,3 +47,23 @@ test_that("can bridge JFRC2 to IS2 using inverse registration", {
 
   expect_equal(points.bridge, points.bridgeexp)
 })
+
+test_that("can use a bridging registration in extra directory",{
+  td=tempfile(pattern = 'extrabridge')
+  dir.create(td)
+  op=options('nat.flybrains.extrabridge'=td)
+  on.exit(options(op))
+
+  library(rgl)
+  library(nat)
+  rcreg=file.path(td,"rhubarb_crumble.list")
+  expect_error(xform_brain(c(1,1,1), ref='rhubarb',sample='crumble') )
+  i=identityMatrix()
+  m=matrix(c(1,1,1),ncol=3)
+  cmtk.mat2dof(identityMatrix(), rcreg)
+  expect_equal(xform_brain(m, ref='rhubarb',sample='crumble'), m)
+
+  expect_message(xform_brain(m, ref='crumble',sample='rhubarb'),
+                 regexp = 'Numerically inverting registration')
+  unlink(td, recursive = TRUE)
+})
