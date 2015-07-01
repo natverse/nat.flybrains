7dc422845513029a82e8a08ab30ddd0bc1afaae1
Gregory Jefferis <jefferis@gmail.com>
2015-07-01 16:46:05 +0100

    you can just downdload a single repo if you want
    * doc polishing

--- R/reg_repos.R
+++ R/reg_repos.R
@@ -1,13 +1,22 @@
-#' Download and install Jefferis lab registrations
+#' Download (or update) and install Jefferis lab registrations
 #'
-#' Uses \code{\link{download_reg_repo}} to download git repositories containing
-#' bridging and mirroring registrations for Drosophila melanogaster, simulans
-#' and virilis template brains.
-#' @seealso \url{https://github.com/jefferislab/BridgingRegistrations}, \url{https://github.com/jefferislab/MirrorRegistrations}, and \url{https://github.com/jefferislab/DrosophilidBridgingRegistrations}.
+#' Uses \code{nat.templatebrains::\link{download_reg_repo}} to download git
+#' repositories containing bridging and mirroring registrations for Drosophila
+#' melanogaster, simulans and virilis template brains.
+#' @seealso \url{https://github.com/jefferislab/BridgingRegistrations},
+#'   \url{https://github.com/jefferislab/MirrorRegistrations}, and
+#'   \url{https://github.com/jefferislab/DrosophilidBridgingRegistrations}.
 #' @export
 #' @seealso \code{\link{download_reg_repo}}
-download_jefferislab_registrations <- function() {
-  repos=c("BridgingRegistrations", "MirrorRegistrations", "DrosophilidBridgingRegistrations")
-
+#' @examples
+#' \dontrun{
+#' # to download just one of these repositories do e.g.:
+#' download_jefferislab_registrations("Mirror")
+#'
+#' # to download your own registration repositories for use do
+#' download_reg_repo("https://github.com/myuser/myrepo")
+#' }
+download_jefferislab_registrations <- function(repos=c("BridgingRegistrations", "MirrorRegistrations", "DrosophilidBridgingRegistrations")) {
+  repos=match.arg(repos, several.ok = TRUE)
   download_reg_repo(paste0("jefferislab/", repos))
 }
