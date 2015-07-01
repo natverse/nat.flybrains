11139f359aeb16bc72f639f109ccdb9ad838695d
Gregory Jefferis <jefferis@gmail.com>
2015-07-01 17:08:36 +0100

    when loading package interactively complain if we haven't found any extra regs

--- R/zzz.r
+++ R/zzz.r
@@ -1,5 +1,13 @@
 .onLoad <- function(libname, pkgname) {
-
+  if(interactive()) {
+    # see if we've got our standard registrations
+    std_repos=c("BridgingRegistrations", "MirrorRegistrations", "DrosophilidBridgingRegistrations")
+    dirs=local_reg_dir_for_url(file.path("https://github.com/jefferislab",std_repos))
+    if(!any(file.exists(dirs)))
+      packageStartupMessage("You do not have any jefferislab extra registrations installed!\n",
+                            "We recommend getting them with:\n\n",
+                            "download_jefferislab_registrations()")
+  }
   # add paths to registrations supplied by package
   regdirs=c("bridgingregistrations", "mirroringregistrations")
   paths=system.file("extdata", regdirs, package = 'nat.flybrains')
