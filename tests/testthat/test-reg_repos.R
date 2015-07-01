08e9c069529f2b470c8e73feee06d7f6a9d0bdba
Gregory Jefferis <jefferis@gmail.com>
2015-07-01 12:21:20 +0100

    move reg_repos code -> nat.templatebrains

--- tests/testthat/test-reg_repos.R
+++
@@ -1,9 +0,0 @@
-context("registration repositories")
-
-test_that("make_reg_url behaves", {
-  expect_equal(make_reg_url("jefferislab/BridgingRegistrations"),
-               "https://github.com/jefferislab/BridgingRegistrations")
-
-  abc=c("a/b", "a/c")
-  expect_equal(make_reg_url(abc), paste0("https://github.com/", abc))
-})
