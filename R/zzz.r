036eb85c9c1fb12185d44488dbec8c7f215e5ab7
Gregory Jefferis <jefferis@gmail.com>
2015-07-01 16:23:18 +0100

    only add this packages reg folders on startup
    * nat.templatebrains handles the rest

--- R/zzz.r
+++ R/zzz.r
@@ -1,19 +1,9 @@
 .onLoad <- function(libname, pkgname) {
 
-  # see if we have any extra registrations downloaded by user
-  # nb prepend these since we want them to be used in place of
-  # those distributed with the package
-  extra_reg=extra_reg_repos()
-  if(length(extra_reg)) {
-    options(nat.templatebrains.regdirs=
-              union(getOption('nat.templatebrains.regdirs'), extra_reg))
-  }
-
   # add paths to registrations supplied by package
   regdirs=c("bridgingregistrations", "mirroringregistrations")
   paths=system.file("extdata", regdirs, package = 'nat.flybrains')
-  # nb this ensures no duplicates
-  options(nat.templatebrains.regdirs=
-            union(getOption('nat.templatebrains.regdirs'), paths))
+  # nb we want them to go after any user supplied registrations
+  add_reg_folders(paths, first=FALSE)
   invisible()
 }
