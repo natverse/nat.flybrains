297365cae25bd507300b34b45a019e48f063e592
James Manton <ajd.manton@googlemail.com>
2015-07-01 11:06:02 +0100

    Add documentation for new objects

--- R/DATA.r
+++ R/DATA.r
@@ -214,3 +214,83 @@ NULL
 #' @docType data
 #' @seealso \code{\link{VNCIS1}}
 NULL
+
+
+#' Template information for the Drosophila melanogaster reference brain
+#'
+#' The Dmel reference brain is a shape averaged template brain generated using
+#' the CMTK avg_adm tool. See
+#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
+#'
+#' @name Dmel
+#' @docType data
+NULL
+
+
+#' Template information for the Drosophila simulans reference brain
+#'
+#' The Dsim reference brain is a shape averaged template brain generated using
+#' the CMTK avg_adm tool. See
+#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
+#'
+#' @name Dsim
+#' @docType data
+NULL
+
+
+#' Template information for the Drosophila virilis reference brain
+#'
+#' The Dvir reference brain is a shape averaged template brain generated using
+#' the CMTK avg_adm tool. See
+#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
+#'
+#' @name Dvir
+#' @docType data
+NULL
+
+
+#' Surface model of the Drosophila melanogaster reference brain
+#'
+#' The Dmel reference brain is a shape averaged template brain generated using
+#' the CMTK avg_adm tool. See
+#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
+#'
+#' @name Dmel.surf
+#' @docType data
+#' @examples
+#' \dontrun{
+#' # Depends on nat
+#' plot3d(Dmel.surf)
+#' }
+NULL
+
+
+#' Surface model of the Drosophila simulans reference brain
+#'
+#' The Dsim reference brain is a shape averaged template brain generated using
+#' the CMTK avg_adm tool. See
+#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
+#'
+#' @name Dsim.surf
+#' @docType data
+#' @examples
+#' \dontrun{
+#' # Depends on nat
+#' plot3d(Dsim.surf)
+#' }
+NULL
+
+#' Surface model of the Drosophila virilis reference brain
+#'
+#' The Dvir reference brain is a shape averaged template brain generated using
+#' the CMTK avg_adm tool. See
+#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
+#'
+#' @name Dvir.surf
+#' @docType data
+#' @examples
+#' \dontrun{
+#' # Depends on nat
+#' plot3d(Dvir.surf)
+#' }
+NULL
