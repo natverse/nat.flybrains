264014281e98586854b7841b0282ff55b76c21f4
Gregory Jefferis <jefferis@gmail.com>
2016-11-01 18:34:11 +0000

    Split up long DATA.r file

---
+++ R/DATA-Drosophilids.r
@@ -0,0 +1,90 @@
+#' Template information for the Drosophila melanogaster reference brain
+#'
+#' The Dmel reference brain is a shape averaged template brain generated using
+#' the CMTK avg_adm tool. See
+#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
+#'
+#' \href{http://dx.doi.org/10.5281/zenodo.10591}{doi:10.5281/zenodo.10591}
+#'
+#' @name Dmel
+#' @docType data
+NULL
+
+
+#' Template information for the Drosophila simulans reference brain
+#'
+#' The Dsim reference brain is a shape averaged template brain generated using
+#' the CMTK avg_adm tool. See
+#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
+#'
+#' \href{http://dx.doi.org/10.5281/zenodo.1059}{doi:10.5281/zenodo.1059}
+#'
+#' @name Dsim
+#' @docType data
+NULL
+
+
+#' Template information for the Drosophila virilis reference brain
+#'
+#' The Dvir reference brain is a shape averaged template brain generated using
+#' the CMTK avg_adm tool. See
+#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
+#'
+#' \href{http://dx.doi.org/10.5281/zenodo.10593}{doi:10.5281/zenodo.10593}
+#'
+#' @name Dvir
+#' @docType data
+NULL
+
+
+#' Surface model of the Drosophila melanogaster reference brain
+#'
+#' The Dmel reference brain is a shape averaged template brain generated using
+#' the CMTK avg_adm tool. See
+#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
+#'
+#' \href{http://dx.doi.org/10.5281/zenodo.10591}{doi:10.5281/zenodo.10591}
+#'
+#' @name Dmel.surf
+#' @docType data
+#' @examples
+#' \dontrun{
+#' # Depends on nat
+#' plot3d(Dmel.surf)
+#' }
+NULL
+
+
+#' Surface model of the Drosophila simulans reference brain
+#'
+#' The Dsim reference brain is a shape averaged template brain generated using
+#' the CMTK avg_adm tool. See
+#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
+#'
+#' \href{http://dx.doi.org/10.5281/zenodo.1059}{doi:10.5281/zenodo.1059}
+#'
+#' @name Dsim.surf
+#' @docType data
+#' @examples
+#' \dontrun{
+#' # Depends on nat
+#' plot3d(Dsim.surf)
+#' }
+NULL
+
+#' Surface model of the Drosophila virilis reference brain
+#'
+#' The Dvir reference brain is a shape averaged template brain generated using
+#' the CMTK avg_adm tool. See
+#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
+#'
+#' \href{http://dx.doi.org/10.5281/zenodo.10593}{doi:10.5281/zenodo.10593}
+#'
+#' @name Dvir.surf
+#' @docType data
+#' @examples
+#' \dontrun{
+#' # Depends on nat
+#' plot3d(Dvir.surf)
+#' }
+NULL
