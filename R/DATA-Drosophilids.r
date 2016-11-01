4e0d10ecfb8d1370c1677dfafcdaef544404f127
Gregory Jefferis <jefferis@gmail.com>
2016-11-01 18:44:32 +0000

    Merge Drosophilid template/surface docs

--- R/DATA-Drosophilids.r
+++ R/DATA-Drosophilids.r
@@ -1,4 +1,4 @@
-#' Template information for the Drosophila melanogaster reference brain
+#' Template information and surface model for D. melanogaster reference brain
 #'
 #' The Dmel reference brain is a shape averaged template brain generated using
 #' the CMTK avg_adm tool. See
@@ -7,45 +7,15 @@
 #' \href{http://dx.doi.org/10.5281/zenodo.10591}{doi:10.5281/zenodo.10591}
 #'
 #' @name Dmel
+#' @family Drosophilid-Brains
 #' @docType data
 NULL
 
-
-#' Template information for the Drosophila simulans reference brain
-#'
-#' The Dsim reference brain is a shape averaged template brain generated using
-#' the CMTK avg_adm tool. See
-#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
-#'
-#' \href{http://dx.doi.org/10.5281/zenodo.1059}{doi:10.5281/zenodo.1059}
-#'
-#' @name Dsim
-#' @docType data
-NULL
-
-
-#' Template information for the Drosophila virilis reference brain
-#'
-#' The Dvir reference brain is a shape averaged template brain generated using
-#' the CMTK avg_adm tool. See
-#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
-#'
-#' \href{http://dx.doi.org/10.5281/zenodo.10593}{doi:10.5281/zenodo.10593}
-#'
-#' @name Dvir
-#' @docType data
-NULL
-
-
-#' Surface model of the Drosophila melanogaster reference brain
-#'
-#' The Dmel reference brain is a shape averaged template brain generated using
-#' the CMTK avg_adm tool. See
-#' \url{https://github.com/jefferislab/MakeAverageBrain} for the relevant code.
-#'
-#' \href{http://dx.doi.org/10.5281/zenodo.10591}{doi:10.5281/zenodo.10591}
+#' The surface model was constructed in Amira by L. Goetz and G. Jefferis using
+#' a simple threshold, followed by a surface simplification to ~ 18,000 faces.
 #'
 #' @name Dmel.surf
+#' @rdname Dmel
 #' @docType data
 #' @examples
 #' \dontrun{
@@ -55,7 +25,7 @@ NULL
 NULL
 
 
-#' Surface model of the Drosophila simulans reference brain
+#' Template information and surface model for D. simulans reference brain
 #'
 #' The Dsim reference brain is a shape averaged template brain generated using
 #' the CMTK avg_adm tool. See
@@ -63,7 +33,17 @@ NULL
 #'
 #' \href{http://dx.doi.org/10.5281/zenodo.1059}{doi:10.5281/zenodo.1059}
 #'
+#' @name Dsim
+#' @family Drosophilid-Brains
+#' @docType data
+NULL
+
+
+#' The surface model was constructed in Amira by L. Goetz and G. Jefferis using
+#' a simple threshold, followed by a surface simplification to ~ 18,000 faces.
+#'
 #' @name Dsim.surf
+#' @rdname Dsim
 #' @docType data
 #' @examples
 #' \dontrun{
@@ -72,7 +52,7 @@ NULL
 #' }
 NULL
 
-#' Surface model of the Drosophila virilis reference brain
+#' Template information and surface model for D. virilis reference brain
 #'
 #' The Dvir reference brain is a shape averaged template brain generated using
 #' the CMTK avg_adm tool. See
@@ -80,7 +60,17 @@ NULL
 #'
 #' \href{http://dx.doi.org/10.5281/zenodo.10593}{doi:10.5281/zenodo.10593}
 #'
+#' @name Dvir
+#' @family Drosophilid-Brains
+#' @docType data
+NULL
+
+
+#' The surface model was constructed in Amira by L. Goetz and G. Jefferis using
+#' a simple threshold, followed by a surface simplification to ~ 18,000 faces.
+#'
 #' @name Dvir.surf
+#' @rdname Dvir
 #' @docType data
 #' @examples
 #' \dontrun{
