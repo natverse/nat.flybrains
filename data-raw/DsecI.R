3b9eb8af661b678c47d61d1709aaa2e1688a992e
Gregory Jefferis <jefferis@gmail.com>
2018-02-05 16:49:14 +0000

    typo

--- data-raw/DsecI.R
+++ data-raw/DsecI.R
@@ -6,7 +6,7 @@ library(devtools)
 use_data(DsecI.surf, overwrite = T)
 
 
-=templatebrain(name = "DsecI", type = NULL,
+DsecI=templatebrain(name = "DsecI", type = NULL,
     sex = NULL, dims = c(768L, 768L, 171L), voxdims = c(0.461258800521512,
     0.461258800521512, 1), origin = c(0, 0, 0), BoundingBox = structure(c(0,
     353.7855, 0, 353.7855, 0, 170), .Dim = 2:3, class = "boundingbox"),
