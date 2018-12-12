3e4c5358932eadc627bb24366d2d565eb8e4d4bc
Gregory Jefferis <jefferis@gmail.com>
2018-12-12 17:20:41 +0000

    Fix shortname of JRC2018F

--- data-raw/jrc2018.R
+++ data-raw/jrc2018.R
@@ -5,9 +5,8 @@ use_data(JRC2018F.surf, overwrite = T)
 
 library(nat.templatebrains)
 JRC2018F <-
-  as.templatebrain(
-    'data-raw/JRC2018_FEMALE_38um_iso_16bit.nrrd',
-    name = 'JRC2018F',
+  as.templatebrain('data-raw/JRC2018_FEMALE_38um_iso_16bit.nrrd',
+    regName = 'JRC2018F',
     type = 'Average template brain',
     sex = 'F',
     doi = 'https://doi.org/10.1101/376384'
